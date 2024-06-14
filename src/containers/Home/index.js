34bd954a54e948ea9693799a2ef1939e54a3dbb3
JoaoVictorrAraujo <joaovictoraraujo298@gmail.com>
2024-06-14 01:07:48 -0300

    feat: Carrossel de Ofertas

--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -1,13 +1,19 @@
 import React from "react";
 import HomeLogo from '../../assets/home-logo.png'
+
 import CategoryCarousel from "../../components/CategoryCarousel";
+import OffersCarousel from "../../components/OffersCarousel";
+
+
 import {Container, HomeImg} from './style'
+
 
 function Home(){
     return(
         <Container>
         <HomeImg src={HomeLogo} alt="logo da home" />
         <CategoryCarousel></CategoryCarousel>
+        <OffersCarousel></OffersCarousel>
         </Container>
     )
 }
